75efb81fbcd13b994d30acdcf1b01190e1635c1e
Ethan Shah <31543210+ethanshah@users.noreply.github.com>
2017-12-14 12:44:31 -0800

    Delete Parameters.m

--- Homework/Parameters.m
+++
@@ -1,9 +0,0 @@
-function Parameters()
- load('Drand.mat');   
- global nsample 
- nsample = length(Drand);
- mu = 1;
- sigma = 10;
- x = fminsearch(@getLogNormal, [mu,sigma]);
- fprintf ('mu: %f, sigma: %f\n', x(1), x(2));
- end
